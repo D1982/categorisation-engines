b1cd31857824ab3e5e32f905f143b3203d6a5bef
D1982 <ottdaniel.ch@gmail.com>
2019-03-28 08:17:32 +0100

    Moved constants to config.py, related code changes and minor bug fixes

--- Categorisation/Common/config.py
+++ Categorisation/Common/config.py
@@ -1,10 +1,10 @@
 """ Configuration items
 
 """
-class CastlightConfig:
 
-    def __init__(self):
-        self.api_headers = dict()
-        # API Subscription key
-        self.api_headers['Ocp-Apim-Subscription-Key'] = '8d2b8e00bc794f7c81fcdcc7359bb995'
+CSV_DELIMITER = ';' # Standard Delimiter
+
+URL = "gateway.castlightfinancial.com"
+
+WAIT = 3
 
