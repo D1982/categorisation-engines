996aca20e0c47584fac6b7aab2ccbe51b135a137
D1982 <ottdaniel.ch@gmail.com>
2019-05-01 17:32:03 +0200

    Added parameter UI_STRING_MAX_WITH, otherwise only documentation + formatting adjustment

--- Categorisation/Common/config.py
+++ Categorisation/Common/config.py
@@ -1,14 +1,15 @@
 """ Configuration items
 
 """
-from enum import Enum
 
-# File settings
+# File processing
 CSV_DELIMITER = ';' # Standard Delimiter
 
 # Proxy settings
 USE_PROXY = False
+
 PROXY_URL = '11.112.142.4'
+
 PROXY_PORT = '8080'
 
 # HTTP settings
@@ -19,6 +20,12 @@ API_URL_CASTLIGHT = 'gateway.castlightfinancial.com'
 
 # Tink specifics
 TINK_LOGFILE = 'tink.log'
+
 API_URL_TINK = 'https://api.tink.se'
+
 IN_FILE_PATTERN_TINK = 'data/TinkRequest_*.csv'
+
 OUT_FILE_PATTERN_TINK = 'data/TinkResponse_*.csv'
+
+# UI
+UI_STRING_MAX_WITH = 100
