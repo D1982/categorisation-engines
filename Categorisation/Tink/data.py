dae22e8a6a96e379b0fd8b5ada33d2de6d02f64c
D1982 <ottdaniel.ch@gmail.com>
2019-05-14 15:47:05 +0200

    Create a new data access layer

---
+++ Categorisation/Tink/data.py
@@ -0,0 +1,50 @@
+import Categorisation.Common.util as util
+import Categorisation.Common.config as cfg
+
+
+import collections
+import logging
+
+
+class TinkDAO:
+
+    def __init__(self):
+        logging.info("Initiated:" + "TinkDAO.__init__()")
+
+        # Data sources
+        self.user_src = 'unbound'
+        self.acc_src = 'unbound'
+        self.trx_src = 'unbound'
+
+        # Field names of interest
+        self.fieldnames_user = ('external_user_id', 'label', 'locale', 'market')
+        self.fieldnames_acc = ('x', 'y', 'z')
+        self.fieldnames_trx = ('x', 'y', 'z')
+
+        # Data collections
+        self.users = collections.OrderedDict()
+        self.accounts = collections.OrderedDict()
+        self.transactions = collections.OrderedDict()
+
+        # File handler utility
+        self.file_handler = util.FileHandler()
+
+    def bind_data_source(self, src_type, locator):
+        if src_type == cfg.TinkEntityType.UserEntity:
+            self.user_src = locator
+        elif src_type == cfg.TinkEntityType.AccountEntity:
+            self.acc_src = locator
+        elif src_type == cfg.TinkEntityType.TransactionEntity:
+            self.trx_src = locator
+
+    def read_users(self):
+        data = self.file_handler.read_csv_file(filename=self.user_src, fieldnames=self.fieldnames_user)
+        return data
+
+    def read_accounts(self):
+        util.FileHandler.read_csv_file(self.acc_src, self.fieldnames_user)
+
+    def read_transactions(self):
+        util.FileHandler.read_csv_file(self.trx_src, self.fieldnames_user)
+
+
