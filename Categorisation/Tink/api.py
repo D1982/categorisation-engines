5b90ae14436a43c5e7445dbd5f08622c82a2b31b
D1982 <ottdaniel.ch@gmail.com>
2019-07-03 17:24:57 +0200

    PEP8 cosmetics (adding more documentation)

--- Categorisation/Tink/api.py
+++ Categorisation/Tink/api.py
@@ -1,6 +1,5 @@
-""" Tink API
+"""Direct access to Tink's API endpoints. """
 
-"""
 import Categorisation.Common.config as cfg
 import Categorisation.Common.secret as secret
 
@@ -9,6 +8,9 @@ import collections
 import logging
 import requests
 import json
+
+"""TinkAPI class."""
+
 
 class TinkAPI:
     def __init__(self):
@@ -20,6 +22,9 @@ class TinkAPI:
         self.partner_info = dict()
         self.partner_info['client_id'] = secret.TINK_CLIENT_ID
         self.partner_info['client_secret'] = secret.TINK_CLIENT_SECRET
+
+
+"""Abstract wrapper class for a request to the Tink API."""
 
 
 class TinkAPIRequest:
@@ -51,8 +56,9 @@ class TinkAPIRequest:
         pass
 
 
-""" Abstract wrapper class for a Tink Response requests.Response object
-"""
+"""Abstract wrapper class for a response object of the Python requests library."""
+
+
 class TinkAPIResponse:
     def __init__(self, response):
         # Data from requests.Response object
@@ -75,9 +81,11 @@ class TinkAPIResponse:
             # This service does not return a JSON so just use the text instead
             self.data.update({'text': response.text})
 
+    """Minimalistic string representation of a TinkAPIResponse instance."""
     def to_string(self):
         return 'RESPONSE: ' + self.reason + ' ({code})'.format(code=str(self.status_code))
 
+    """Extended string representation of a TinkAPIResponse instance."""
     def to_string_formatted(self):
         text = self.to_string()
 
@@ -91,13 +99,14 @@ class TinkAPIResponse:
 
         return text + os.linesep*2
 
-    # Abstract Methods
+    """Abstract method."""
     def to_string_custom(self):
         pass
 
 
-""" Wrapper class for the Tink monitoring service
-"""
+"""Wrapper class for the Tink monitoring service."""
+
+
 class MonitoringService(TinkAPI):
     def __init__(self):
         super().__init__()
@@ -135,6 +144,10 @@ class MonitoringService(TinkAPI):
         resp = TinkAPIResponse(response)
         return req, resp
 
+
+"""Wrapper class for the Tink category service."""
+
+
 class CategoryService(TinkAPI):
     def __init__(self):
         super().__init__()
@@ -156,10 +169,14 @@ class CategoryService(TinkAPI):
         return req, resp
 
 
+"""Wrapper class for the Tink user service."""
+
+
 class UserService(TinkAPI):
     def __init__(self):
         super().__init__()
 
+    """Create a new user in the Tink platform."""
     def activate_user(self, ext_user_id, label, market, locale, client_access_token):
         # Target API specifications
         url = self.url_root
@@ -191,32 +208,54 @@ class UserService(TinkAPI):
 
         return req, resp
 
-    def delete_user(self, ext_user_id):
-        pass
 
-    def get_user(self, ext_user_id):
+    """Delete an existing user in the Tink platform."""
+    def delete_user(self, user_id, ext_user_id, client_access_token):
         # Target API specifications
         url = self.url_root
-        method = 'GET'
-        postfix = '/api/v1/user'
+        method = 'POST'
+        postfix = '/api/v1/user/delete'
+        # Request headers
+        req = TinkAPIRequest(method=method, endpoint=url+postfix)
+        req.headers.update({'X-Tink-OAuth-Client-ID': secret.TINK_CLIENT_ID})
+        req.headers.update({'Authorization': 'Bearer ' + client_access_token})
+        # Request body remains empty
+
+        # Log Request
+        logging.debug('POST {dest} using data {data}'.format(
+                dest=req.endpoint, data=req.data))
+        # Fire the request against the API endpoint
+        response = requests.post(url=req.endpoint, data=json.dumps(req.data), headers=req.headers)
+        # Process the response
+        resp = UserDeleteResponse(response)
+        # Log the result depending on the HTTP status code
+        if resp.content and resp.status_code == 204:
+            logging.debug('RESPONSE from {dest} Response => {data}'.format(
+                    dest=req.endpoint, data=str(resp.to_string())))
+        else:
+            logging.debug('RESPONSE from {dest} not as expected => {msg}'.format(
+                    dest=req.endpoint, msg=resp.to_string()))
+
+        return req, resp
+
+
+"""Wrapper class for the Tink OAuth service."""
+
 
 class OAuthService(TinkAPI):
     def __init__(self):
         super().__init__()
 
-    """ 
-    Authorize access to the client (company account) 
-    Documentation: https://docs.tink.com/enterprise/api/#get-an-authorization-token
-    Purpose​: This will return an API token (valid only for the authenticated client) 
-             that can be used to manipulate the users tied to your clientId. 
-             Remember that your YOUR_CLIENT_SECRET should be kept a secret!
-    Response​: ​Access Token Response for a client which expires after 30 mins 
-             (no refresh token provided, use the same endpoint again to get a 
-             new access token). Please note that this token must also be kept a 
-             secret and not exposed to any public client.
-
+    """Authorize access to the client (company account).
+    
+    https://docs.tink.com/enterprise/api/#get-an-authorization-token
+    
+    This method will store an access token (valid only for the authenticated client) 
+    that can be used to manipulate the users tied to secret.TINK_CLIENT_ID. 
+    The access token usually expires after 30 mins and can be renewed with an
+    appropriate refresh token that should also be kept as a secret (like the client secret)
     """
-    def authorize_client_access(self, grant_type, scope):
+    def authorize_client_access(self, grant_type, scope, delete_dict=None):
         # Target API specifications
         url = self.url_root
         method = 'POST'
@@ -228,6 +267,16 @@ class OAuthService(TinkAPI):
         req.data.update({'client_secret': secret.TINK_CLIENT_SECRET})
         req.data.update({'grant_type': grant_type})
         req.data.update({'scope': scope})
+
+        # If delete_dict is provided then the service has to be used to delete data
+        # This is a workaround that can be used in order to delete existing users and accounts
+        if delete_dict:
+            if 'external_user_id' in delete_dict:
+                req.data.update({'external_user_id': delete_dict['external_user_id']})
+            elif 'user_id' in delete_dict:
+                req.data.update({'user_id': delete_dict['user_id']})
+        #TODO: Add accounts in case of deletion for accounts is also working
+
         # Log Request
         logging.debug('POST {dest} using data {data}'.format(
                 dest=req.endpoint, data=req.data))
@@ -245,11 +294,9 @@ class OAuthService(TinkAPI):
 
         return req, resp
 
-
-
-    """ 
-    Grant access to a user 
-    https://docs.tink.com/enterprise/api/#create-an-authorization-for-the-given-user-id-wit h-requested-scopes
+    """Grant access to a user.
+    
+    https://docs.tink.com/enterprise/api/#create-an-authorization-for-the-given-user-id-with-requested-scopes
     """
     def grant_user_access(self, client_access_token, user_id, ext_user_id, scope='user:read'):
         # Target API specifications
@@ -282,8 +329,8 @@ class OAuthService(TinkAPI):
 
         return req, resp
 
-    """ 
-    Get the OAuth access token 
+    """Get the OAuth access token for a user.
+    
     ​https://api.tink.se/api/v1/oauth/token
     """
     def get_oauth_access_token(self, code, ):
@@ -317,6 +364,10 @@ class OAuthService(TinkAPI):
 
         return req, resp
 
+
+"""Abstract wrapper class for a AuthenticationResponse from Tink's OAuth service."""
+# TODO: Check if there might be a better naming for this class
+
 class OAuth2AuthenticationTokenResponse(TinkAPIResponse):
 
     def __init__(self, response):
@@ -327,6 +378,9 @@ class OAuth2AuthenticationTokenResponse(TinkAPIResponse):
         # Get relevant data out of the JSON => Facilitates string formatting for UI outputs
         self.data = {key: value for key, value in self.json.items() if key in self.names}
 
+
+"""Abstract wrapper class for a AuthorizeResponse from Tink's OAuth service."""
+# TODO: Check if there might be a better naming for this class
 
 class OAuth2AuthorizeResponse(TinkAPIResponse):
 
@@ -343,7 +397,29 @@ class OAuth2AuthorizeResponse(TinkAPIResponse):
             self.scope = self.data['code']
 
 
+"""Abstract wrapper class for UserActivationResponse from Tink's user service."""
+
+
 class UserActivationResponse(TinkAPIResponse):
+
+    def __init__(self, response):
+        super().__init__(response)
+
+        # Define fields of interest referring to the official API documentation
+        self.names = {'user_id', 'errorMessage', 'errorCode'}
+
+        # Save relevant data in dedicated member variables => Facilitates data flow
+        if self.status_code == 204:
+            self.user_id = self.data['user_id']
+
+            # Get relevant data out of the JSON => Facilitates string formatting for UI outputs
+            self.data = {key: value for key, value in self.json.items() if key in self.names}
+
+
+"""Abstract wrapper class for UserDeleteResponse from Tink's user service."""
+
+
+class UserDeleteResponse(TinkAPIResponse):
 
     def __init__(self, response):
         super().__init__(response)
