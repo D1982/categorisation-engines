2e2849d4b89e21f54518236427a1370e5bf510d2
D1982 <ottdaniel.ch@gmail.com>
2019-07-29 15:18:43 +0200

    Added a TODO to raise exception NotImplementedError in abstract methods of base-class TinkAPIResponse

--- Categorisation/Tink/api.py
+++ Categorisation/Tink/api.py
@@ -172,6 +172,11 @@ class TinkAPIResponse(metaclass=abc.ABCMeta):
         :return: a formatted, human readable string representation of the data
         within an instance of this class
         """
+        # TODO: Raise exception NotImplementedError
+        # This exception is derived from RuntimeError.
+        # In user defined base classes, abstract methods should raise this exception
+        # when they require derived classes to override the method, or while the class
+        # is being developed to indicate that the real implementation still needs to be added.
         return ''
 
     def http_status(self, group: cfg.HTTPStatusCode = None):
